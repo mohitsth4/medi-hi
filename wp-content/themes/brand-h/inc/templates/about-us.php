897dc2b4b58d3cae3143e5e80fac50676800693e
mohitsth4 <37899955+mohitsth4@users.noreply.github.com>
2020-08-28 01:48:00 +0530

    new all changes

---
+++ wp-content/themes/brand-h/inc/templates/about-us.php
@@ -0,0 +1,312 @@
+<!-- Section 3-6 (start) -->
+<div class="hidden">
+you can not see this because it is hidden 
+</div>
+<div class="flex-wrap">
+
+	<div class="flex-wrap relative mt-5" style="background-image:url('<?php bloginfo('template_directory') ?>/img/static/p_himedi_1_m.jpg'); background-repeat:no-repeat; background-position:center top; background-size:cover; min-height:360px; margin-top:70px;">
+		<!--<img class="w-full" src="<?php //bloginfo('template_directory') ?>/img/static/p_himedi_1_m.jpg" alt="">-->
+		<div class="top-0 right-0  flex-wrap items-center justify-center content-center w-full h-full p-6"
+			style="background-color: rgba(0, 0, 0, 0.4); padding-top:60px; padding-bottom:140px;">
+			<h2 class="font-bold text-center px-6 font-size-24 leading-relaxed text-white" style="font-size:22px">
+				About Himedi
+			</h2>
+			<p class="pt-4 px-2 text-gray-700 text-center text-2xl lg:font-size-24 leading-none text-white" style="color:white; line-height:1.2">
+				Himedi makes known excellent medical technology and service of
+				Korea and address inconveniences in medical tourism so as to contribute to a happy and healthy life
+				of foreign patients who need treatment as well as the development of Korea’s medical tourism
+				industry.</p>
+		</div>
+	</div>
+
+	<div class="flex justify-center content-center flex-wrap w-full">
+		<img class=" m-6 mt-20" src="<?php bloginfo('template_directory') ?>/img/logo_h.png" alt="" style="width:195px">
+	</div>
+
+	<div class="flex flex-wrap m-2 mx-6">
+		<p class="pt-4 px-2 text-gray-700 text-left text-3xl leading-none text-white" style="color:#5F5F5F; line-height:1.2" >
+			‘Himedi’ is a combined word of ‘Hi’ which is a friendly greeting and ‘Medical’ which
+			means medical science. <br><br> It reflects the intention to provide friendly service that wipes out fear from
+			foreign patients coming from overseas.</p>
+	</div>
+
+	<div class="flex flex-wrap w-full px-3 pt-10 mt-40" style="background-color: #f7f7f7;" id="history">
+		<h3 class="text-center px-0 font-size-24 leading-relaxed" style="font-size:20px">
+			History
+		</h3>
+		<p class="pt-4 px-2 text-gray-700 text-left text-2xl leading-none text-white" style="color:#5F5F5F; line-height:1.2" >
+			The path that Himedi has followed since 2011 as ‘an enterprise of innovation that
+			realizes easy and comfortable medical tourism.’</p>
+		<div class="flex mt-2 pb-4 border-solid border-b-2 border-gray-200 leading-relaxed text-base" style="color:#666!important;">
+			<div class="text-sm font-bold mr-4" style="color:#333!important;">
+				2020
+			</div>
+
+			<div class="flex flex-wrap ">
+				<div class="flex mb-2">
+					<div class="text-sm mr-4">
+						04
+					</div>
+					<div class="text-sm ">
+						Attracted Series B investment amounting to 6 billion won
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+
+		<div class="flex mt-2 pb-4 border-solid border-b-2 border-gray-200">
+			<div class="text-sm font-bold mr-4" style="color:#333!important;">
+				2019
+			</div>
+
+			<div class="flex flex-wrap">
+				<div class="flex mb-2">
+					<div class="text-sm mr-4">
+						04
+					</div>
+					<div class="text-sm">
+						Himedi Korea Co., Ltd. Separated as an affiliate
+					</div>
+				</div>
+
+				<div class="flex mb-2">
+					<div class="text-sm mr-4">
+						04
+					</div>
+					<div class="text-sm">
+						Changed the company name to Himedi Co., Ltd.
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="flex mt-2 pb-4 border-solid border-b-2 border-gray-200 leading-relaxed">
+			<div class="text-sm font-bold mr-4" style="color:#333!important;">
+				2018
+			</div>
+
+			<div class="flex flex-wrap">
+				<div class="flex mb-2">
+					<div class="text-sm mr-4 ">
+						10
+					</div>
+					<div class="text-sm">
+						Attracted Series A investment amounting to 2 billion won from Murex Partners
+					</div>
+				</div>
+
+				<div class="flex mb-2">
+					<div class="text-sm mr-4">
+						09
+					</div>
+					<div class="text-sm">
+						Concluded the concierge contract of Russian patients with Severance Check-up
+					</div>
+				</div>
+
+				<div class="flex mb-2">
+					<div class="text-sm mr-4">
+						08
+					</div>
+					<div class="text-sm">
+						Founded the Uzbekistan corporation of Himedi
+					</div>
+				</div>
+
+				<div class="flex mb-2">
+					<div class="text-sm mr-4">
+						08
+					</div>
+					<div class="text-sm">
+						Concluded the concierge operation/management contract of Arab Muslim patients with BOBATH
+						Memorial Hospital
+					</div>
+				</div>
+
+				<div class="flex mb-2">
+					<div class="text-sm mr-4">
+						06
+					</div>
+					<div class="text-sm">
+						Concluded the concierge operation/management contract of Arab Muslim patients with Wooridul
+						Hospital in Cheongdam
+					</div>
+				</div>
+
+				<div class="flex mb-2">
+					<div class="text-sm mr-4">
+						02
+					</div>
+					<div class="text-sm">
+						Concluded the concierge operation/management contract of Arab Muslim patients with Seoul National University Bundang Hospital
+					</div>
+				</div>
+
+				<div class="flex mb-2">
+					<div class="text-sm mr-4">
+						01
+					</div>
+					<div class="text-sm">
+						Concluded the concierge operation/management contract of Arab Muslim patients with Seoul National University Hospital (Main)
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="flex mt-2 pb-4 border-solid border-b-2 border-gray-200 leading-relaxed">
+			<div class="text-sm font-bold mr-4" style="color:#333!important;">
+				2015
+			</div>
+
+			<div class="flex flex-wrap">
+				<div class="flex mb-2">
+					<div class="text-sm mr-4">
+						12
+					</div>
+					<div class="text-sm">
+						Concluded the concierge operation/management contract of Arab Muslim patients with Cha University Gangnam Medical Center
+					</div>
+				</div>
+
+				<div class="flex mb-2">
+					<div class="text-sm mr-4">
+						11
+					</div>
+					<div class="text-sm">
+						Concluded the concierge operation/management contract of Arab Muslim patients with Samsung Medical Center
+					</div>
+				</div>
+
+				<div class="flex mb-2">
+					<div class="text-sm mr-4">
+						09
+					</div>
+					<div class="text-sm">
+						Concluded the UAE health insurance patient registration/billing service contract with Daman Health Insurance
+					</div>
+				</div>
+
+				<div class="flex mb-2">
+					<div class="text-sm mr-4">
+						03
+					</div>
+					<div class="text-sm">
+						An internship program with Arabic Department of Chosun University
+					</div>
+				</div>
+
+				<div class="flex mb-2">
+					<div class="text-sm mr-4">
+						03
+					</div>
+					<div class="text-sm">
+						An internship program with Arabic Studies Department, Myongji University
+					</div>
+				</div>
+				
+			</div>
+		</div>
+
+		<div class="flex mt-2 pb-4 border-solid border-b-2 border-gray-200 leading-relaxed">
+			<div class="text-sm font-bold mr-4" style="color:#333!important;">
+				2014
+			</div>
+
+			<div class="flex flex-wrap">
+				<div class="flex mb-2">
+					<div class="text-sm mr-4">
+						12
+					</div>
+					<div class="text-sm">
+						Concluded the medical checkup service contract for the staff of the Russian airline Air Bridge Cargo
+					</div>
+				</div>
+
+				<div class="flex mb-2">
+					<div class="text-sm mr-4">
+						07
+					</div>
+					<div class="text-sm">
+						Founded the corporation of Widers Korea
+					</div>
+				</div>
+				
+			</div>
+		</div>
+
+		<div class="flex mt-2 pb-4 border-solid border-b-2 border-gray-200 leading-relaxed">
+			<div class="text-sm font-bold mr-4" style="color:#333!important;">
+				2011
+			</div>
+
+			<div class="flex flex-wrap">
+				<div class="flex mb-2">
+					<div class="text-sm mr-4">
+						12
+					</div>
+					<div class="text-sm">
+						Concluded the concierge operation/management contract of Arab Muslim patients with Severance Hospital
+					</div>
+				</div>
+
+				<div class="flex mb-2">
+					<div class="text-sm mr-4">
+						12
+					</div>
+					<div class="text-sm">
+						Founded Widers Company
+					</div>
+				</div>
+				
+			</div>
+		</div>
+
+	</div>
+
+	<div class="flex flex-wrap w-full px-6 py-5">
+		<h3 class="font-bold text-center px-0 font-size-24 leading-relaxed" style="font-size:22px">
+			Location</h3>
+
+		<iframe class="w-full h-56 mb-4" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDgl81NrLLD43y1Qep_Dc8INyr8ilgvSwE
+	&q=Space+Needle,Seattle+WA" allowfullscreen>
+		</iframe>
+
+		<h3 class="font-bold text-center px-0 font-size-24 leading-relaxed" style="font-size:22px">
+			Himedi Headquarters</h3>
+
+		<div class="leading-relaxed">
+			<table>
+				<tr class="text-sm">
+					<td class="pr-4 py-2" valign="top">Address</td>
+					<td class="py-2">7th Floor, Hongwu-2-Building, 78-gil 22, Seochodaero, Seocho-gu, Seoul</td>
+				</tr>
+
+				<tr class="text-sm">
+					<td class="pr-4 py-2" valign="top">Location</td>
+					<td class="py-2">5 minutes from Exit No. 6 of Gangnam Station (Shinbundang subway line)</td>
+				</tr>
+
+				<tr class="text-sm">
+					<td class="pr-4 py-2" valign="top">Tel</td>
+					<td class="py-2">070-8146-8986</td>
+				</tr>
+
+				<tr class="text-sm">
+					<td class="pr-4 py-2" valign="top">Fax</td>
+					<td class="py-2">02-545-0420</td>
+				</tr>
+
+				<tr class="text-sm">
+					<td class="pr-4 py-2" valign="top">E-mail</td>
+					<td class="py-2">acc@himedi.com</td>
+				</tr>
+
+			</table>
+		</div>
+
+	</div>
+
+</div>
+<!-- Section 3-6 (end) -->
---
+++ wp-content/themes/brand-h/inc/templates/affiliate.php
@@ -0,0 +1,166 @@
+
+<div class="pt-20 relative">
+  <div id="popup_cancel_btn" class="absolute top-0 right-0 p-4 m-4 text-3xl cursor-pointer z-40 font-bold text-white hidden" style="top:70px;" >X</div>
+  <div id="pop_up_bg" class="absolute w-screen-98 h-screen-per top-0 right-poop-0 z-20 hidden" style="background-color: rgba(0, 0, 0, 0.5);"></div>
+  <div id="pop_up_bg2" class="absolute w-screen h-screen top-0 right-0 z-20 hidden" style="background-color: rgba(0, 0, 0, 0.5);"></div>
+  <div id="popup_cancel_btn2" class="h-screen h-full absolute top-0 right-0 p-4 m-4 text-3xl cursor-pointer z-40 font-bold text-white hidden" >X</div>
+  
+  <!--Hidden-popup better (start)  -->
+  <div id="popup" class="hidden z-40 absolute top-0 " style="top:25%" >
+    <div class="py-10 px-10 mx-5 bg-white flex flex-col rounded" style="z-index:1">  
+      <div alt="hospital image"
+                class="bg-cover content-partners"
+                style="background-image: url(<?php bloginfo('template_directory') ?>/img/partners/01.jpg);">
+      </div>
+      <div class="flex justify-center">
+        <div class="" style="margin-top:-60px;border-radius:50%;border: 1px solid rgba(0,0,0,.1);z-index:1">
+            <img  src="<?php bloginfo('template_directory') ?>/img/partners/01-logo.png" width="92" height="92" alt="hospital logo" class="bg-white rounded-full ">
+        </div>
+      </div>
+      <div class="w-full flex-1">
+        <div class="font-size-22 text-b80 text-center mb-5" style="margin-top: 10px;"> CHA Gangnam
+              Medical Center
+        </div>
+        <div class="text-base leading-normal">
+          IVF, Additional treatments for IVF, IUI, Male Infertility, Fertility Tests for Men and Women
+        </div>
+        <div class="font-size-16 text-b50 text-center margin-28 h-line-1-4 font-light keep-direction leading-none text-base"
+              style="margin: 8px 28px 14px;">#IVF #Infertilityspecialtyhospital #Infertilitycounseling
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--Hidden-popup better (end)  -->
+  <!--Hidden-popup better (start)  -->
+  <div id="popup2" class="hidden z-40 absolute top-0" style="top:15%" >
+    <div class="py-10 px-10 mx-5 bg-white flex flex-col" style="z-index:1">  
+      <div alt="hospital image"
+                class="bg-cover content-partners"
+                style="background-image: url(<?php bloginfo('template_directory') ?>/img/partners/02.jpg);">
+      </div>
+      <div class="flex justify-center">
+        <div class="" style="margin-top:-60px;border-radius:50%;border: 1px solid rgba(0,0,0,.1);z-index:1">
+            <img  src="<?php bloginfo('template_directory') ?>/img/partners/02-logo.png" width="92" height="92" alt="hospital logo" class="bg-white rounded-full ">
+        </div>
+      </div>
+      <div class="w-full flex-1">
+        <div class="font-size-22 text-b80 text-center mb-5" style="margin-top: 10px;"> MizMedi Hospital</div>
+        <div class="text-base leading-normal">
+        Polycystic Ovary Syndrome/Ovulation Disorder, Ovarian Dysfunction, Male Infertility, IUI, IVF, Selection of Best Embryo, Creating the Optimum Condition for Implantation, Recurrent Miscarriage/Recurrent Implantation Failure, Preimplantation Genetic Diagnosis, Fertility Preservation (Egg Freezing), Laparoscopy, Hysteroscopy
+        </div>
+        <div class="font-size-16 text-b50 text-center margin-28 h-line-1-4 font-light keep-direction leading-none text-base"
+              style="margin: 8px 28px 14px;">#OB-GYN #Women'sHospital #IVF
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--Hidden-popup better (end)  -->
+  <!--Hidden-popup better (start)  -->
+  <div id="popup3" class="hidden z-40 absolute top-0" style="top:15%" >
+    <div class="py-10 px-10 mx-5 bg-white flex flex-col" style="z-index:1">  
+      <div alt="hospital image"
+                class="bg-cover content-partners"
+                style="background-image: url(<?php bloginfo('template_directory') ?>/img/partners/03.jpg);">
+      </div>
+      <div class="flex justify-center">
+        <div class="" style="margin-top:-60px;border-radius:50%;border: 1px solid rgba(0,0,0,.1);z-index:1">
+            <img  src="<?php bloginfo('template_directory') ?>/img/partners/03-logo.png" width="92" height="92" alt="hospital logo" class="bg-white rounded-full ">
+        </div>
+      </div>
+      <div class="w-full flex-1">
+        <div class="font-size-22 text-b80 text-center mb-5" style="margin-top: 10px;"> Seoul National University Bundang Hospital</div>
+        <div class="text-base leading-normal">
+        
+Test: Spine X-rays, Spine MRI, Spine CT, Dexa bone densitometry, Pre-operative tests, Knee X-rays (knee series & teleradiogram), Knee MRI, Shoulder X-rays, Rotator cuff X-rays, Dexa bone densitometry, Shoulder CT, Shoulder MRI, Ultrasound Extremity, GU Kidney & bladder CT, Kidney SPECT/CT, Chest CT, Bone scan, Mammography, Breast ultrasound, breast gun biopsy, other types of biopsy (needle or punch)
+Tests for metastasis: Breast MRI, chest CT, abdomen & pelvis CT, PET scan, bone scan, pre-op labs
+
+Surgery: Interbody fusion, C-spine or L-spine, Posterior fusion, Total Knee Replacement Arthroplasty and other arthroscopic, Total shoulder arthroplasty, Robot-assisted Partial Nephrectomy or Radical Nephrectomy, Total mastectomy, In Vitro Fertilization
+
+        </div>
+        <div class="font-size-16 text-b50 text-center margin-28 h-line-1-4 font-light keep-direction leading-none text-base"
+              style="margin: 8px 28px 14px;">#smarthospital #minimally-invasive-surgery #international-accredited
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--Hidden-popup better (end)  -->
+ <!--Hidden-popup better (start)  -->
+ <div id="popup4" class="hidden z-40 absolute top-0" style="top:15%" >
+    <div class="py-10 px-10 mx-5 bg-white flex flex-col" style="z-index:1">  
+      <div alt="hospital image"
+                class="bg-cover content-partners"
+                style="background-image: url(<?php bloginfo('template_directory') ?>/img/partners/04.jpg);">
+      </div>
+      <div class="flex justify-center">
+        <div class="" style="margin-top:-60px;border-radius:50%;border: 1px solid rgba(0,0,0,.1);z-index:1">
+            <img  src="<?php bloginfo('template_directory') ?>/img/partners/04-logo.png" width="92" height="92" alt="hospital logo" class="bg-white rounded-full ">
+        </div>
+      </div>
+      <div class="w-full flex-1">
+        <div class="font-size-22 text-b80 text-center mb-5" style="margin-top: 10px;"> The Catholic Univ. of Korea Seoul St.Mary's Hospital</div>
+        <div class="text-base leading-normal">
+        
+        Robotic-assisted myomectomy and reconstructive surgery, Robotic-assisted total laparoscopic hysterectomy, Preoperative examination(MRI)
+
+
+        </div>
+        <div class="font-size-16 text-b50 text-center margin-28 h-line-1-4 font-light keep-direction leading-none text-base"
+              style="margin: 8px 28px 14px;">#JCI&AAHRPP #WorldclassMedicalstaff #state-of-the-art-systems
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--Hidden-popup better (end)  -->
+
+
+  <div style="margin-top:70px;">
+    
+    
+    <div class="border-gray-700 border w-16 h-16 rounded-full py-2 px-2 flex items-center justify-center text-gray-700" style="margin:0 auto">
+        <img class="mx-auto lg:h-8 h-8 w-8 lg:w-8"  src="<?php bloginfo('template_directory') ?>/img/hospital-alt-light.svg" >
+      </div>
+    
+  </div>
+  <div class="w-full mb-32 ">
+    <h2 class="font-bold text-center px-6 font-size-24 leading-relaxed " style="font-size:22px">
+      Check out Korea’s <br class="lg:hidden">best medical staff.
+    </h2>
+  </div>
+  <!-- logo --> 
+  <div class="mt-20 main_cont_img">
+    <div class="flex">
+      <img onclick="myFunction1()" id="click_me_btn" class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/01-logo.png" >
+      <img onclick="myFunction2()" id="click_me_btn2" class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/02-logo.png" >
+      <img onclick="myFunction3()" id="click_me_btn3" class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/03-logo.png" >
+    </div>
+    <div class="flex">
+      <img onclick="myFunction4()" id="click_me_btn4" class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/04-logo.png" >
+      <img class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/05-logo.png" >
+      <img class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/06-logo.png" >
+    </div>
+    <div class="flex">
+      <img class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/07-logo.png" >
+      <img class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/08-logo.png" >
+      <img class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/09-logo.png" >
+    </div>
+    <div class="flex">
+      <img class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/10-logo.png" >
+      <img class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/11-logo.png" >
+      <img class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/12-logo.png" >
+    </div>
+    <div class="flex">
+      <img class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/13-logo.png" >
+      <img class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/14-logo.png" >
+    </div>
+  </div>
+  <!-- logo end -->
+          
+          <!-- <div id="popup_cancel_btn2" class="absolute top-0 right-0 p-4 m-4 text-3xl cursor-pointer z-40 font-bold text-white hidden" style="top:10%">X</div> -->
+          <!-- <div id="pop_up_bg2" class="absolute w-screen h-screen top-0 right-0 z-20 hidden" style="background-color: rgba(0, 0, 0, 0.5);"></div> -->
+      <!--Hidden-popup better (start)  -->
+</div>
+  
+
+  
+
+  
